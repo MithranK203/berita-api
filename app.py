464fe921122731905404fe7e489c5b7f8e0ed41d
MithranK203 <mithrankhiryan@gmail.com>
2026-07-24 11:34:40 +0700

    Support Render deployment

--- app.py
+++ app.py
@@ -30,5 +30,8 @@ def berita():
         }), 500
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
